974568d823427dbc5d6041e97894d979bef5ba2d
vsulimovvv <super.sulimov2014@yandex.ru>
2023-03-16 16:28:59 +0300

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -421,7 +421,53 @@ window.addEventListener('DOMContentLoaded', () => {
     'active'
   );
   someTabs('.types-tabs', '.types-tabs__btn', '.types-tabs__content', 'active');
-  someTabs(
+
+  // * ===== Toggle Tabs
+  function hoverTabs(
+    headerSelector,
+    tabSelector,
+    contentSelector,
+    activeClass
+  ) {
+    const header = document.querySelectorAll(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const content = document.querySelectorAll(contentSelector);
+
+    header.forEach((el) => {
+      if (el) {
+        hideTabContent();
+        showTabContent();
+        function hideTabContent() {
+          content.forEach((item) => {
+            item.classList.remove('active');
+          });
+          tab.forEach((item) => {
+            item.classList.remove(activeClass);
+          });
+        }
+        function showTabContent(i = 0) {
+          content[i].classList.add('active');
+          tab[i].classList.add(activeClass);
+        }
+        header.forEach((item) => {
+          if (item) {
+            item.addEventListener('mouseover', (e) => {
+              const target = e.target;
+              if (target.classList.contains(tabSelector.replace(/\./, ''))) {
+                tab.forEach((item, i) => {
+                  if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+  hoverTabs(
     '.categories-tabs',
     '.categories-tabs-btn',
     '.categories-tabs-content',
